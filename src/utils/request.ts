ac5731da2c6e3b68b997754cafc760d4e279b8e5
Graziele Oliveira <hypixelgrazi@gmail.com>
2022-09-10 20:32:24 -0300

    add request helper function

---
+++ src/utils/request.ts
@@ -0,0 +1,22 @@
+import {
+  AxiosError,
+  AxiosRequestConfig,
+  AxiosResponse,
+} from "axios";
+import { client } from "../config/api";
+
+
+export const request = async (config: AxiosRequestConfig<any>) => {
+  const onSuccess = (response: AxiosResponse) => {
+    const {
+      data: { results },
+    } = response;
+    return results;
+  };
+
+  const onError = (error: AxiosError) => {
+    return Promise.reject(error.message);
+  };
+
+  return client(config).then(onSuccess).catch(onError)
+};
