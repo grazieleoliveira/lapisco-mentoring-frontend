d91190362f034bd3dcea7e6c68c964ef1322ca25
Graziele Oliveira <hypixelgrazi@gmail.com>
2022-09-10 20:31:35 -0300

    fix urls

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
-import { TMDB_IMG_BASE_URL } from "../constants/apiUrls";
+import { TMDB_BASE_URL } from "../constants/apiUrls";
 import { apiToken } from "../constants/token";
 
-export default axios.create({
-  baseURL: TMDB_IMG_BASE_URL,
+export const client = axios.create({
+  baseURL: TMDB_BASE_URL,
   headers: {
     "Content-Type": "application/json;charset=utf-8",
     Authorization: `Bearer ${apiToken}`,
--- src/constants/apiUrls.ts
+++ src/constants/apiUrls.ts
@@ -1,2 +1,2 @@
-export const TMDB_IMG_BASE_URL = "https://image.tmdb.org/t/p/original/";
-export const TMDB_BASE_URL = "https://api.themoviedb.org/3/";
+export const TMDB_IMG_BASE_URL = "https://image.tmdb.org/t/p/original";
+export const TMDB_BASE_URL = "https://api.themoviedb.org/3";
