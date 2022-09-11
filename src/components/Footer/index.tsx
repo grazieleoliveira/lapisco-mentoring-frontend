47b704a6593c5d982fa7d802b36749b25928ec35
Graziele Oliveira <hypixelgrazi@gmail.com>
2022-09-11 18:37:55 -0300

    reference dribble design

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,7 +3,15 @@ import * as S from "./styles";
 export const Footer = () => {
   return (
     <S.Container>
-      <S.FooterText>Made with ❤️ by Grazi 干梦希</S.FooterText>
+      <S.FooterText>
+        Made with ❤️ by Grazi 干梦希, <br /> Inspired by{" "}
+        <a
+          href={`https://dribbble.com/shots/16126116-Netflix-TV-App-Concept?utm_source=Clipboard_Shot&utm_campaign=DenisBashlaev&utm_content=Netflix%20TV%20App%20Concept&utm_medium=Social_Share&utm_source=Clipboard_Shot&utm_campaign=DenisBashlaev&utm_content=Netflix%20TV%20App%20Concept&utm_medium=Social_Share`}
+        >
+          Dis Bashlaev{" "}
+        </a>
+        on Dribble
+      </S.FooterText>
     </S.Container>
   );
 };
