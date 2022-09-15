5dd9a1d5dbf88853bfbdeda2d0b44d2a842f47f1
Graziele Oliveira <hypixelgrazi@gmail.com>
2022-09-15 15:56:39 -0300

    fix my chinese name lmao

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,7 +4,7 @@ export const Footer = () => {
   return (
     <S.Container>
       <S.FooterText>
-        Made with ❤️ by Grazi 干梦希, <br /> Inspired by{" "}
+        Made with ❤️ by Grazi 甘梦希, <br /> Inspired by{" "}
         <a
           href={`https://dribbble.com/shots/16126116-Netflix-TV-App-Concept?utm_source=Clipboard_Shot&utm_campaign=DenisBashlaev&utm_content=Netflix%20TV%20App%20Concept&utm_medium=Social_Share&utm_source=Clipboard_Shot&utm_campaign=DenisBashlaev&utm_content=Netflix%20TV%20App%20Concept&utm_medium=Social_Share`}
         >
